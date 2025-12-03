ac7e5ddb9859d278cae20e4d784133e3b1fceeb1
JohnieWalkerCZ <jan.prochazka.cr@gmail.com>
2025-12-03 17:49:54 +0100

    Fixed error with balance being sometimes set to 0

--- src/app/[camp_url]/layout.tsx
+++ src/app/[camp_url]/layout.tsx
@@ -8,64 +8,65 @@ import { fetcher, FetchError } from "@/lib/fetch";
 import { CampDetails } from "@/types";
 
 export default function CampLayout({
-	children,
+    children,
 }: {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-	const { data: session, update, status } = useSession();
-	const params = useParams();
-	const pathname = usePathname();
-	const router = useRouter();
-	const [isSessionReady, setIsSessionReady] = useState(false);
+    const { data: session, update, status } = useSession();
+    const params = useParams();
+    const pathname = usePathname();
+    const router = useRouter();
+    const [isSessionReady, setIsSessionReady] = useState(false);
 
-	const campUrl = params.camp_url as string;
+    const campUrl = params.camp_url as string;
 
-	const updateSessionWithCampData = useCallback(async () => {
-		try {
-			const campData = await fetcher<CampDetails>(
-				`/api/camps/${campUrl}`
-			);
+    const updateSessionWithCampData = useCallback(async () => {
+        try {
+            const campData = await fetcher<CampDetails>(
+                `/api/camps/${campUrl}`
+            );
 
-			await update({
-				camp_url: campData.name_url,
-				camp_id: campData.id,
-				user: {
-					...session?.user,
-					is_admin: campData.is_admin,
-					is_org: campData.is_org,
-				},
-			});
-			setIsSessionReady(true);
-		} catch (error) {
-			if (error instanceof FetchError) {
-				router.push("/unauthorized");
-			} else {
-				console.error("Error updating session with camp data:", error);
-			}
-		}
-	}, [campUrl, router, session?.user, update]);
+            await update({
+                camp_url: campData.name_url,
+                camp_id: campData.id,
+                user: {
+                    ...session?.user,
+                    balance: session?.user.balance || undefined,
+                    is_admin: campData.is_admin,
+                    is_org: campData.is_org,
+                },
+            });
+            setIsSessionReady(true);
+        } catch (error) {
+            if (error instanceof FetchError) {
+                router.push("/unauthorized");
+            } else {
+                console.error("Error updating session with camp data:", error);
+            }
+        }
+    }, [campUrl, router, session?.user, update]);
 
-	useEffect(() => {
-		const checkSession = async () => {
-			if (status === "loading") return;
+    useEffect(() => {
+        const checkSession = async () => {
+            if (status === "loading") return;
 
-			if (session && campUrl && session.camp_url !== campUrl) {
-				await updateSessionWithCampData();
-			} else if (session) {
-				setIsSessionReady(true);
-			}
-		};
+            if (session && campUrl && session.camp_url !== campUrl) {
+                await updateSessionWithCampData();
+            } else if (session) {
+                setIsSessionReady(true);
+            }
+        };
 
-		checkSession();
-	}, [session, campUrl, status, updateSessionWithCampData]);
+        checkSession();
+    }, [session, campUrl, status, updateSessionWithCampData]);
 
-	if (pathname.includes("login") || pathname.includes("signup")) {
-		return <>{children}</>;
-	}
+    if (pathname.includes("login") || pathname.includes("signup")) {
+        return <>{children}</>;
+    }
 
-	if (!isSessionReady) {
-		return <Loader />;
-	}
+    if (!isSessionReady) {
+        return <Loader />;
+    }
 
-	return <>{children}</>;
+    return <>{children}</>;
 }
--- src/app/api/balance/route.ts
+++ src/app/api/balance/route.ts
@@ -37,39 +37,39 @@ import { BalanceResponse } from "@/types";
  *                   example: Unauthorized
  */
 export async function GET(
-	req: NextRequest
+    req: NextRequest
 ): Promise<NextResponse<BalanceResponse | { error: string }>> {
-	const session = await getServerSession(authOptions);
+    const session = await getServerSession(authOptions);
 
-	if (!session) {
-		return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-	}
+    if (!session) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
 
-	const searchParams = req.nextUrl.searchParams;
-	const campName = searchParams.get("camp");
+    const searchParams = req.nextUrl.searchParams;
+    const campName = searchParams.get("camp");
 
-	if (!session.camp_id && !campName) {
-		return NextResponse.json({ balance: 0 }, { status: 200 });
-	}
+    if (!session.camp_id && !campName) {
+        return NextResponse.json({ balance: 0 }, { status: 200 });
+    }
 
-	let campId = session.camp_id;
-	if (campName && !session.camp_id) {
-		const camp = await prisma.camp.findUnique({
-			where: { name_url: campName },
-			select: { id: true },
-		});
-		if (camp) {
-			campId = camp.id;
-		}
-	}
+    let campId = session.camp_id;
+    if (campName && !session.camp_id) {
+        const camp = await prisma.camp.findUnique({
+            where: { name_url: campName },
+            select: { id: true },
+        });
+        if (camp) {
+            campId = camp.id;
+        }
+    }
 
-	const balance = await prisma.balance.findFirst({
-		where: {
-			user: Number(session.user.id),
-			camp: campId || -1,
-		},
-		select: { amount: true },
-	});
+    const balance = await prisma.balance.findFirst({
+        where: {
+            user: Number(session.user.id),
+            camp: campId || -1,
+        },
+        select: { amount: true },
+    });
 
-	return NextResponse.json({ balance: balance?.amount || 0 });
+    return NextResponse.json({ balance: balance?.amount || 0 });
 }
